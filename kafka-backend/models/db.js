5dd57600a505550c8617c077ab4df54e04a6b873
Chris Li <hychrisli@users.noreply.github.com>
2018-04-15 19:06:10 -0700

    Add connection pool

--- kafka-backend/models/db.js
+++ kafka-backend/models/db.js
@@ -5,13 +5,21 @@ const db = process.env.MONGODB;
 const user = process.env.MONGODB_USER;
 const pass = process.env.MONGODB_PASS;
 
-
-mongoose.connect(db, {user, pass}, (err) => {
+mongoose.createConnection(db, {user, pass, poolSize: 20}, (err) => {
   if (err)
     console.log("Mongo DB Connection Error", err);
   else
     console.log("Mongo DB Connection Ready!");
 });
+
+
+
+/*mongoose.connect(db, {user, pass, poolSize: 20}, (err) => {
+  if (err)
+    console.log("Mongo DB Connection Error", err);
+  else
+    console.log("Mongo DB Connection Ready!");
+});*/
 
 module.exports = mongoose;
 
