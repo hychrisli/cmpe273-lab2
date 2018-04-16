3031967f2fe1e5ea86ff278f64c745882d6debb7
Chris Li <hychrisli@users.noreply.github.com>
2018-04-15 19:49:10 -0700

    Change Connection

--- kafka-backend/models/db.js
+++ kafka-backend/models/db.js
@@ -5,21 +5,12 @@ const db = process.env.MONGODB;
 const user = process.env.MONGODB_USER;
 const pass = process.env.MONGODB_PASS;
 
-mongoose.createConnection(db, {user, pass, poolSize: 20}, (err) => {
+mongoose.connect(db, {user, pass, poolSize: 20}, (err) => {
   if (err)
     console.log("Mongo DB Connection Error", err);
   else
     console.log("Mongo DB Connection Ready!");
 });
-
-
-
-/*mongoose.connect(db, {user, pass, poolSize: 20}, (err) => {
-  if (err)
-    console.log("Mongo DB Connection Error", err);
-  else
-    console.log("Mongo DB Connection Ready!");
-});*/
 
 module.exports = mongoose;
 
